b1e2a5c74105fa52b4be9bf3327ef405f177b238
gkdl <limyoung92@naver.com>
2024-12-19 21:53:29 +0900

    ascas

--- api-logging/src/main/java/com/logging/api/annotation/LogExecutionTime.java
+++ api-logging/src/main/java/com/logging/api/annotation/LogExecutionTime.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME) 
+@Retention(RetentionPolicy.RUNTIME)  
 public @interface LogExecutionTime {
     boolean includeArgs() default true;  // 메서드 인자 포함 여부
 }
