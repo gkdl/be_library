864484bb6cedd2f6529b9cfa66470ee3284cbaaa
gkdl <limyoung92@naver.com>
2024-12-19 21:17:17 +0900

    ttt

--- examples/src/main/java/com/logging/api/config/WebConfig.java
+++ examples/src/main/java/com/logging/api/config/WebConfig.java
@@ -7,12 +7,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class WebConfig {
 
-    @Bean(name="logFormat")
-    public String logFormat() {
-        return "dasdasd";
-    }
-
-
     /**
      *  logExecutionStrategy bean 등록시 커스텀한 LogExecutionStrategy 인터페이스 실행
      */
